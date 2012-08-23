d65b731cf97a03ed41dc4df5254992cbe4db79f4
Javier Rosa <javirosa@eecs.berkeley.edu>
2012-08-23 15:44:13 +1000

    UNTESTED: refactored runcheck.py

--- runcheck.py
+++ runcheck.py
@@ -1,50 +1,54 @@
 import time, os, subprocess
+from sys import maxint as MAXINT
+
 MINUTES = 15;
 SENDMINUTES = 15;
 
-
-if os.path.exists('/tmp/time'):	
-	f=open('/tmp/time', 'r')
-	readtime = f.readline().strip()
-	difference = time.mktime(time.localtime()) - float(readtime)
-	print difference
-	f.close()
-else:
-	difference = MINUTES * 60 + 1
+LOOPTIMER = '/tmp/time'
+SENDMAILTIMER = '/tmp/sendtime'
 
 
-if difference > MINUTES * 60:
-	
-	tempfile = os.popen("pgrep -f tcpdump")
+"""Using pgrep searches for processes which have "key" as a substring."""
+def getPIDList(key):
+	tempfile = os.popen("pgrep -f " + key)
 	pidlist = tempfile.readlines()
 	tempfile.close()
-	tempfile = os.popen("pgrep -f loop.py")
-	pidlist = pidlist + tempfile.readlines()
-	tempfile.close()
+	return pidlist
+
+"""First asks process to stop and waits. Forcefully kills the process after the waittime."""
+def killProcesses(pidlist,waitTime=10):
 	for x in pidlist:
 		os.popen("kill "+x)
-	time.sleep(10)
+	time.sleep(waitTime)
 	for x in pidlist:
 		os.popen("kill -9 "+ x)
+
+"""Get's the amount of time since the timer was set. Returns the given unsetdefault value if the timer was not set."""
+def timeElapsed(timer,unsetdefault=MAXINT):
+	if os.path.exists(timer):	
+		f=open(timer, 'r')
+		readtime = f.readline().strip()
+		f.close()
+		difference = time.mktime(time.localtime()) - float(readtime)
+		print difference
+		return difference
+	else:
+		return unsetdefault
+
+difference = timeElapsed(LOOPTIMER,MINUTES*60+1)
+
+#If loop.py hasn't updated its timer in awhile kill and restart it
+if difference > MINUTES * 60:
+	pidlist = getPIDList("tcpdump") + getPIDList("loop.py")
+	killProcesses(pidlist)
 	os.popen("python loop.py")
 
-if os.path.exists('/tmp/sendtime'):	
-	f=open('/tmp/sendtime', 'r')
-	readtime = f.readline().strip()
-	difference = time.mktime(time.localtime()) - float(readtime)
-	f.close
-else:
-	difference = SENDMINUTES * 60 + 1
+#If loop.py hasn't updated its timer in awhile kill and restart it
+difference = timeElapsed(SENDMAILTIMER,SENDMINUTES*60+1)
 
 if difference > SENDMINUTES * 60:
-	tempfile = os.popen("pgrep -f sendmail.py")
-	pidlist = tempfile.readlines()
-	tempfile.close()
-	for x in pidlist:
-		os.popen("kill "+x)
-	time.sleep(10)
-	for x in pidlist:
-		os.popen("kill -9 "+ x)
+	pidlist = getPIDList("sendmail.py")
+	killProcesses(pidlist)
 	os.popen("python sendmail.py")
 
 #	subprocess.Popen("killall -q tcpdump".split(" "), shell=False)
@@ -56,13 +60,7 @@ if difference > SENDMINUTES * 60:
 #	subprocess.Popen("killall -9 -q foo".split(" "), shell=False)
 	
 #	subprocess.Popen()
-tempfile = os.popen("pgrep -f sendmail.py")
-pidlist = tempfile.readlines()
-tempfile.close()
+pidlist = getPIDList("sendmail.py")
 if len(pidlist)>1:
-	for x in pidlist:
-		os.popen("kill "+x)
-	time.sleep(10)
-	for x in pidlist:
-		os.popen("kill -9 "+ x)
+	killProcesses(pidlist)
 	
